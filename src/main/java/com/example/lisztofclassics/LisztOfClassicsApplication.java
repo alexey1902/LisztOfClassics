f5cebdbfdff8ced1bccfb3736aaef3e68174947c
alexey1902 <alesha_1902@mail.ru>
2021-12-29 13:19:08 +0300

    Diplom tms

---
+++ src/main/java/com/example/lisztofclassics/LisztOfClassicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lisztofclassics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisztOfClassicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LisztOfClassicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lisztofclassics/LisztOfClassicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lisztofclassics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisztOfClassicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
